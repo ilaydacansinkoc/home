33f3ec2bc970b88666a3de6b3863d8ba4462008f
= <=>
2020-02-19 21:58:59 +0300

    Updates

---
+++ precache-manifest.7b86686bd0dbeb4545a33c2af7878a49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da6afe093a817436d2ef0db6ad94ca6",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a7cbabcd4e70a1ca205c",
+    "url": "/home/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a43d9dbd9b57ce1b302",
+    "url": "/home/static/js/2.15d41c2d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/home/static/js/2.15d41c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7cbabcd4e70a1ca205c",
+    "url": "/home/static/js/main.e8000d21.chunk.js"
+  },
+  {
+    "revision": "e27c0f90f1286344ecc1",
+    "url": "/home/static/js/runtime-main.64cdd66f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
